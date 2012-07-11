4aeedfc0cad5d525830fdc39ac6ef4ba2dd9e14c
Esparta <esparta@localhost.(none)>
2012-07-10 20:21:59 -0400

    preparing prep-mysql clean-up

--- prep-mysql.sh
+++ prep-mysql.sh
@@ -4,36 +4,51 @@
 ## Developer: Espartaco Palma (esparta@gmail.com)
 ## Date: 2012-07-04
 ## License: GPL
-## Description: This script try to download and pre-process the
+## Description: This script pre-process the
 ##    database of the Programa de Resultados Electorales Preliminares
 ##    of the Mexico Federal elections.
-##    Optional: Create directory
-##              Prefered downloading server
-##
 
-## Now we are going to pre-process the three databases
-## removing some strings that should be nulls or numbers
-## these are "Ilegible", "Sin dato", "null" or spaces
+## MySQL Settings
+DATABASE="prep2012"
+USER="root"
+HOST="localhost"
 
 ## Working directory
 ## Thanks to stackoverflow
 ## http://stackoverflow.com/a/246128
 
-## TODO: Pass the working directory
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-  files=( presidente.txt senadores.txt diputados.txt )
-  strings=( Ilegible 'Sin dato' null )
+## Working directory
+
+
+## We are going to pre-process the three databases
+## removing some strings that should be nulls or numbers
+## these are "Ilegible", "Sin dato", "null" or spaces
+
+
+
+files=( presidente.txt senadores.txt diputados.txt )
+strings=( Ilegible 'Sin dato' null )
 
   for f in "${files[@]}"; do
     echo  "... pre-procesando base de datos $f"
      for s in "${strings[@]}"; do
         echo -e "\t quitando $s"
-        sed -i "s/$s/\\\N/g" $WDIR/$f
+        sed -i "s/$s/\\\N/g" $DIR/$f
      done
      echo -e "\t quitando | |"
-     sed -i "s/| |/|\\\N|/g" $WDIR/$f
-     sed -i "s/| |/|\\\N|/g" $WDIR/$f
+     sed -i "s/| |/|\\\N|/g" $DIR/$f
+     sed -i "s/| |/|\\\N|/g" $DIR/$f
   done
 
-## mysqlimport --local  --fields-terminated-by="|" --ignore-lines=6 prep2012 -u root -p /home/esparta/presidente.txt
+#mysqlimport --local  --fields-terminated-by="|" --ignore-lines=6 $DATABASE -u $USER -h $HOST -p presidente.txt
 
